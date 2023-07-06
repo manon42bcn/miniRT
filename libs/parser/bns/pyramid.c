821ae8a02b3c6d0c2232f637c1897247cd47fcfc
Manuel Porras Ojeda <manuel.porrasojeda.practicas@telefonica.es>
2023-07-06 23:44:34 +0200

    parser bonus files

---
+++ libs/parser/bns/pyramid.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pyramid.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mporras- <manon42bcn@yahoo.com>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/12 20:18:06 by mporras-          #+#    #+#             */
+/*   Updated: 2023/06/24 21:55:25 by mporras-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "parse.h"
+
+void	inp_pyramid(t_mrt *mrt)
+{
+	mrt->obj = object_builder(PYRAMID, mrt->obj);
+	mrt->obj->elm.sq.centre = get_v3d(mrt->tab[PYR_CENTRE],
+			V3D_COOR);
+	mrt->obj->elm.sq.side = ft_atolf(mrt->tab[PYR_SIDE]);
+	check_range(mrt->obj->elm.sq.side, 0, INFINITY, "Pyramid side");
+	get_common(mrt, PYR_SIDE, "Cube");
+}
