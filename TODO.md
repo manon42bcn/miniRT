629f67e9bb0a40f198ffd6e4d8ce4afda10ef17a
Manuel Porras Ojeda <manuel.porrasojeda.practicas@telefonica.es>
2023-07-22 15:50:27 +0200

    working at hooks

---
+++ TODO.md
@@ -0,0 +1,3 @@
+* [ ] Terminar de separar la funcionalidad mandatory y bonus.
+* [ ] Terminar de implementar traslaciones y rotaciones.
+* [ ] Refactor hooks.
--- libs/solvers/srcs/plane.c
+++ libs/solvers/srcs/plane.c
@@ -1,20 +1,20 @@
 #include "solvers.h"
 
-double			plane_hit(t_v3d origin, t_v3d dir, t_v3d plane_centre, t_v3d plane_dir)
+double	plane_hit(t_v3d origin, t_v3d dir, t_v3d plane_centre, t_v3d plane_dir)
 {
 	double	point;
 	double	base;
 
 	base = ft_dot_v3d(plane_dir, dir);
-	if (fabs(base) < EPSILON)
+	if (base == 0)
 		return (INFINITY);
 	point = (ft_dot_v3d(plane_dir, ft_minus_v3d(plane_centre, origin))) / base;
-	if (point > 0)
+	if (point > EPSILON)
 		return (point);
 	return (INFINITY);
 }
 
-double			plane_solver(t_v3d origin, t_v3d dir, t_obj *obj)
+double	plane_solver(t_v3d origin, t_v3d dir, t_obj *obj)
 {
 	return (plane_hit(origin, dir, obj->elm.pl.centre, obj->normal));
 }
