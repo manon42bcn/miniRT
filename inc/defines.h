a566b08261cbdcddfd514dff37da7c7eea4af313
Manuel Porras Ojeda <manuel.porrasojeda.practicas@telefonica.es>
2023-09-09 16:06:51 +0200

    CMakelists and starting changes on desings

---
+++ inc/defines.h
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   defines.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mporras- <manon42bcn@yahoo.com>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/04 23:56:02 by mporras-          #+#    #+#             */
+/*   Updated: 2023/09/04 23:56:04 by mporras-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef DEFINES_H
+# define DEFINES_H
+
+# define EPSILON 0.00001
+# define SQRT2_2 0.7071067811865476
+# define RAD_ANGLE 0.0174533
+# define TO_RENDER 0
+# define RENDERED 1
+# define RIGHT_CLICK 1
+# define LEFT_CLICK 2
+# define SEP " \t\n"
+# define IDX_ERR	-1
+# define IDX_COMMENT -2
+# define ERROR 1
+# define SUCCESS 0
+# define ROTATE_X 0
+# define ROTATE_Y 1
+# define ROTATE_Z 2
+# define TRUE	1
+# define FALSE	0
+# define MATCH	0
+
+# define CHECKBOARD 1
+# define WAVES 2
+# define RAINBOW 3
+# define REFLECTION_LIMIT 3
+# define ERROR 1
+# define SUCCESS 0
+
+#endif
+
